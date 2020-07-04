c7ca67ae23ea9fb5855abb888a1ec1b7b74f4077
jason1 <javendanob@intergrupo.com>
2020-07-04 17:28:23 -0500

    routing

---
+++ BlazorProducts.Client/BlazorApp1.Client/Components/Component.razor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Client.Components
+{
+    public partial class Component
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> Attributes { get; set; }
+
+        [CascadingParameter]
+        public string Color { get; set; }
+
+        [Parameter]
+        public RenderFragment FragmentoHtml { get; set; }
+
+    }
+}
---
+++ BlazorProducts.Client/BlazorApp1.Client/Pages/Error.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Client.Pages
+{
+    public partial class Error
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        public void Accion()
+        {
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}
